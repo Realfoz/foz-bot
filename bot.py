efe1bf4b5a6abc3dcb8e59b9ebc1b6f53cc05dd1
realfoz <scottpool@hotmail.com>
2025-06-03 17:13:39 +0100

    added basic bot function and a simple test echo command

--- bot.py
+++ bot.py
@@ -1,5 +1,39 @@
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 import os
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
+channel_id = int(os.getenv("CHANNEL_ID"))
+
+intents = discord.Intents.default()  # basic intents, might customise later
+bot = commands.Bot(command_prefix='!', intents=intents)
+intents.message_content = True
+
+@bot.event
+async def on_ready():
+
+    print("Hello World! Bot is ready and connected.") # confirmation in terminal
+    
+     
+    channel = bot.get_channel(channel_id)
+
+    if channel:
+        await channel.send("Foz-Bot is now online! 🤖")
+    else:
+        print("⚠️ Channel not found. Check the channel ID.")
+
+@bot.command()
+async def test(ctx, *, arg): # basic echo test
+    await ctx.send(arg)
+
+
+def run_bot():
+    bot.run(token)
+    ctx.send("Foz-Bot is now online")
+
+
+if __name__ == "__main__":
+    run_bot()
+
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # will be the main file running any logic and general main flow stuff
+
+from bot import *
+
+
+def main():
+    run_bot()
+    test()
+
+
+
+if __name__ == "__main__":
+    main()
